90e64ba9d692574f0f5f2fc6a074d0558c637433
Yogesh Upadhyay <53992168+YogeshUpdhyay@users.noreply.github.com>
2026-01-12 18:36:19 +0000

    update card and website

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -1,12 +1,13 @@
 import "./Card.css"
+import OutlinedSvgText from '../outlineSvgText/OutlineSvgText'
 
-const Card = ({ image, title, alt, style, border }) => {
+const Card = ({ image, title, style, border, svgTop, svgBottom, svgLeft, svgRight, svgTransform, svgText }) => {
     const containerStyle = style || {};
-    const imgStyle = { width: '100%', objectFit: 'cover' };
+    const imgStyle = { width: '100%', objectFit: 'cover', flexGrow: 1 };
     if (containerStyle.height) {
-        imgStyle.height = '100%';
+        imgStyle.height = containerStyle.height;
     } else {
-        imgStyle.height = 'auto';
+        imgStyle.height = '100%';
     }
     
     if (border) {
@@ -15,6 +16,20 @@ const Card = ({ image, title, alt, style, border }) => {
 
     return (
         <div className="card">
+            {svgText && <div style={{
+                position: 'absolute',
+                top: svgTop == '' ? 'auto': svgTop,
+                bottom: svgBottom == '' ? 'auto': svgBottom,
+                left: svgLeft == '' ? 'auto': svgLeft,
+                right: svgRight == '' ? 'auto': svgRight,
+                transform: svgTransform== '' ? '': svgTransform,
+            }}>
+                <OutlinedSvgText 
+                    lines={[svgText]}
+                    fill="black"
+                    stroke="offwhite"
+                />
+            </div>}
             <img 
                 src={image} 
                 alt="cardImage" 
