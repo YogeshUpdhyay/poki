0ef183a047ffd97b4b5c553328c1a274ffe88416
Krishank Shah <krishankshh54@gmail.com>
2026-01-24 20:18:18 +0530

    Work SVGs & Let's Collab Buttons Gap

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -158,14 +158,14 @@ const Work = () => {
                             <img 
                                 src={pinkFlower} 
                                 alt="pink flower decoration" 
-                                className="flowerDecoration flower-left"
+                                className="flowerDecoration flower-pink"
                             />
                         )}
                         {rowIndex === 3 && (
                             <img 
                                 src={pistaFlower} 
                                 alt="pista flower decoration" 
-                                className="flowerDecoration flower-right"
+                                className="flowerDecoration flower-yellow"
                             />
                         )}
                         
